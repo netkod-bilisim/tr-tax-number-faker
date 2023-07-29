642089cd5a3d427b544d8de95c0240682f902ec4
remzi.dalyan <rdalyan16@posta.pau.edu.tr>
2023-07-29 18:26:58 +0300

    Format editing for StyleCI.

--- src/TrTaxNumberFaker.php
+++ src/TrTaxNumberFaker.php
@@ -10,7 +10,7 @@ function trTaxNumberFaker(): string
     $sum = 0;
     for ($i = 0; $i < 9; $i++) {
         $tmp = ($digits[$i] + 10 - ($i + 1)) % 10;
-        $sum = ($tmp == 9 ? $sum + $tmp : $sum + (($tmp * (pow(2, 10 - ($i + 1)))) % 9));
+        $sum = ($tmp == 9 ? $sum + $tmp : $sum + (($tmp * pow(2, 10 - ($i + 1))) % 9));
     }
     $digits[9] = (10 - ($sum % 10)) % 10;
 
